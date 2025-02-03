710cceca03aba0c6bb54e68ea8b4ca62bf6f0e46
@ks <anoop.ks007@gmail.com>
2025-02-03 16:44:18 +0100

    updated view pages

--- django-proj/mysite/urls.py
+++ django-proj/mysite/urls.py
@@ -19,7 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
-    path("polls/owner", include("polls.urls")),
     path("admin/", admin.site.urls),
 
 ]
